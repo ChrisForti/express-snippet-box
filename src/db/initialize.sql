37f003d219c607363b2d5fad7841a8e40c94cb3f
Christopher Forti <christopher.forti.79@gmail.com>
2024-10-04 20:23:23 -0400

    Got ready to start manipulating data.

--- src/db/initialize.sql
+++ src/db/initialize.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users(
-    id SERIAL,
+    id SERIAL primary key,
     email text UNIQUE NOT NULL,
     first_name varchar(25) NOT NULL,
     last_name varchar(25)
@@ -8,14 +8,18 @@ CREATE TABLE IF NOT EXISTS users(
 /* Homework: Is another table called snippets,
  what ius the data we need, 
  lookup foreign key relationship.
-We need a way track the users??? */
+We need a way track the users??? 
+user_id bigint NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+ snippet_owner integer not null,
+    foreign key (snippet_owner) references users (id),*/
 
 CREATE TABLE IF NOT EXISTS snippets(
-    title varchar(100) not null,
+    title varchar(255) not null,
     creation_date integer default extract (epoch from now()) not null,
-    expiration_date integer default extract (epoch from now() + interval '1 month') not null,
+    expiration_date integer not null,
     snippet_id uuid default gen_random_uuid() primary key not null,
-    snippet_owner integer not null,
-    foreign key (snippet_owner) references users (id),
+    user_id bigint NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     content text not null
 );
+
+/*in project find a way to connect ourselves*/
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { pool } from "./db/db.js";
 
 const app = express();
 
