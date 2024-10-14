ad70ba3865f8db179a3de44f8b378e9fed793127
Christopher Forti <christopher.forti.79@gmail.com>
2024-10-14 18:42:55 -0400

    Cleaned up try block, just need to work catch block now.

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,10 +8,11 @@ userRouter.post("/", createUser);
 // handler & route to create new user
 async function createUser(req: Request, res: Response) {
   const { email, firstName, lastName, password, emailVerified } = req.body;
-  const emailRx ="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
+  const emailRx =
+    "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+
   if (!email.match(emailRx)) {
-    //email.match https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
-    // data validation
+    // data validation (see link below for documentation for emailRX)
     return res.status(400).json({ message: "email is missing." });
   }
   if (!firstName) {
@@ -30,16 +31,20 @@ async function createUser(req: Request, res: Response) {
     // data validation
     return res.status(400).json({ message: "email not verified." });
   }
+
   try {
-    const newUser = await pool.query("INSERT INTO users(id, email, firstName, lastName, password, email_verified) VALUES ($1, $2, $3, $4) RETURNING *");
-    res.status(201).json(newUser.rows[0])
+    // SQL prep for creating new user
+    const newUser = await pool.query(
+      "INSERT INTO users(id, email, firstName, lastName, password, email_verified) VALUES ($1, $2, $3, $4) RETURNING *",
+      [email, firstName, lastName, password]
+    );
+    res.status(201).json(newUser.rows[0]);
   } catch (error) {
     console.error("error creating user:", error);
     throw error;
   }
 }
 
-export  { userRouter };
-// create a user handler and validation conditrionals for all not nulls in initialize.sql
-// figure out how to use this string mdn docs match or matches
-//const emailRx = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+export { userRouter };
+// create a user handler and validation conditionals for all not nulls in initialize.sql
+//email.match(emailRx) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
