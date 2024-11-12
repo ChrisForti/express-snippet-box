f422957ee6ddd8ef51dd5ed57963799e604dc560
Christopher Forti <christopher.forti.79@gmail.com>
2024-11-11 19:14:51 -0500

    Made a validator for password and name.

--- src/models/validator.ts
+++ src/models/validator.ts
@@ -18,6 +18,26 @@ export function validateEmail(email: string) {
 } // need to be wrapped in a try catch block when used
 
 export function validatePassword(password: string) {
+  if (password === null) {
+    throw new Error("Invalid password. Password is required");
+  }
+  if (!password) {
+    throw new Error("password is missing");
+  }
+  if (password.length < 8 || password.length > 500) {
+    throw new Error("password must be between 8, and 500 characters");
+  }
   // password logic here, and all other validators to be turned into methods.
   // the password logic should only be what db cares about.
+}
+
+export function validateName(firstName: string, lastName: string) {
+  if (firstName || lastName) {
+    // validate that first and last does not exist already
+    throw new Error("Name already exists");
+  }
+  if (!firstName || !lastName) {
+    // validate first and last are present
+    throw new Error("First an last name is required");
+  }
 }
