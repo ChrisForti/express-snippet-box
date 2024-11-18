4df36cc48443b449f88d040fd2b0c3ed071064e4
Christopher Forti <christopher.forti.79@gmail.com>
2024-11-18 18:53:43 -0500

    Started building snippet model, and import validator functions.

--- src/models/snippet-model.ts
+++ src/models/snippet-model.ts
@@ -1,5 +1,11 @@
 import type pg from "pg";
 import assert from "assert";
+import {
+  validateContent,
+  validateExperationDate,
+  validateTitle,
+  validateUserId,
+} from "./validators.js";
 
 type SnippetModel = {
   tile: string;
@@ -16,7 +22,19 @@ export class snippet {
     assert(!!pool, "Database connection is required");
     this.pool = pool;
   }
-  async createSnippet() {}
+  async createSnippet(
+    title: string,
+    content: string,
+    expirationDate: string,
+    userId: number
+  ) {
+    try {
+      validateTitle(title);
+      validateContent(content);
+      validateExperationDate(expirationDate);
+      validateUserId(userId);
+    } catch (error) {}
+  }
 }
 
 // Create a model here like the other models.
