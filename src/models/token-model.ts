4af4dec4692f42d3a1d9c5129edd62c0a8fed0f1
Christopher Forti <christopher.forti.79@gmail.com>
2024-12-01 14:55:06 -0500

    Updated token maodel to accomadate `base64url`.

--- src/models/token-model.ts
+++ src/models/token-model.ts
@@ -8,7 +8,7 @@ export class Tokens {
     this.pool = pool;
   }
   async generateAuthenticationToken(userId: number) {
-    const plaintext = randomBytes(32).toString();
+    const plaintext = randomBytes(32).toString("base64url");
     const hash = createHash("sha256").update(plaintext).digest("hex");
     const expiry = Math.trunc(Date.now() / 1000) + 60 * 60 * 24 * 30;
 
