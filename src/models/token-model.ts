fc6dea6f19c9be3ee51940870646133bb52fce57
Christopher Forti <christopher.forti.79@gmail.com>
2024-12-08 18:16:15 -0500

    Typo in sql statement within token-model/ generateAuthToken.

--- src/models/token-model.ts
+++ src/models/token-model.ts
@@ -12,7 +12,7 @@ export class Tokens {
     const hash = createHash("sha256").update(plaintext).digest("hex");
     const expiry = Math.trunc(Date.now() / 1000) + 60 * 60 * 24 * 30;
 
-    const sql = "INSERT into tokens (hash, epiry, user_id)VALUES ($1, $2, $3)";
+    const sql = "INSERT into tokens (hash, expiry, user_id)VALUES ($1, $2, $3)";
     const params = [hash, expiry, userId];
     await this.pool.query(sql, params);
 
