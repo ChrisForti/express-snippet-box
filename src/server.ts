276266c13e99ae50beb39f9146d8b56d28658479
Christopher Forti <christopher.forti.79@gmail.com>
2024-12-09 18:40:23 -0500

    Implemented aurhenticate accross all tiers.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { authenticate } from "./middleware/auth.js";
 
 //create express app
 const app = express();
@@ -7,14 +8,17 @@ const PORT = process.env.PORT ?? 3000;
 //Setup middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(authenticate);
 
 app.get("/hello", (req, res) => {
-  res.json({ serverMessage: "Hello world!" });
+  console.log(req.user, "This is the user");
+  // res.json({ serverMessage: "Hello world!" });
 });
 
 import { rootRouter } from "./controllers/root.js";
 import { userRouter } from "./controllers/user-controller.js";
 import { snippetRouter } from "./controllers/snippet-controller.js";
+import { authenticate } from "./middleware/auth.js";
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
